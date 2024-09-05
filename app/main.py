f242511b4cf5b5bbf64680e9e36eb094703bd26a
Bobi <140453994+bobimobisai@users.noreply.github.com>
2024-09-05 15:02:53 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,4 +8,4 @@ import uvicorn
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
